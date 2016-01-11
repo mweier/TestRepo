06c692fb991503adbcbf0dbb796d8f476bae72ea
mweier <michaelgweier@gmail.com>
2016-01-11 08:56:51 -0700

    Added a new message to C#.

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World2");
+            Console.WriteLine("This is an edit to my file.");
             Console.ReadLine();
         }
     }
